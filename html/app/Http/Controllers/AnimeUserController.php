05b0944917992fb797c2c90d770d6f9fd9c4394b
Florian P <palabostf@gmail.com>
2020-11-13 15:59:38 +0100

    [features][rules-status-animes-user][WIP] almost all rules done
    todo want-to-watch-->like=false & watch= false (already done in service back)

--- html/app/Http/Controllers/AnimeUserController.php
+++ html/app/Http/Controllers/AnimeUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\AnimeUser;
 use App\Services\AnimeService;
 use http\Env\Response;
 use Illuminate\Http\Request;
@@ -27,8 +28,11 @@ class AnimeUserController extends Controller
             $user = auth()->user();
             if (!empty($request->json('anime_id')) && !empty($user)) {
                 $animeId = $request->json('anime_id');
-                if($this->animeService->saveUserAnimeStatus($animeId, $user->id, $property)) {
-                    return \response()->json(['message'=> 'property updated'],201);
+
+                /** @var AnimeUser $animeUser */
+                $animeUser = $this->animeService->saveUserAnimeStatus($animeId, $user->id, $property);
+                if($animeUser->wasChanged() || $animeUser->wasRecentlyCreated) {
+                    return \response()->json(['data'=> $animeUser],201);
                 }
                 else {
                     return \response()->json(['message'=> 'cant change property'], 500);
