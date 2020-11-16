4a6ad02538d095f785427d85096eb66a2452f6ef
Florian P <palabostf@gmail.com>
2020-11-16 18:00:21 +0100

    [test][model][Anime][WIP] user : test anime model
    relation anime user

--- html/tests/Unit/AnimeModelTest.php
+++ html/tests/Unit/AnimeModelTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Anime;
+use App\AnimeUser;
 use App\Genre;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -61,9 +62,22 @@ class AnimeModelTest extends TestCase
      * test relationship between Anime & User with pivot table anime_user
      * @test
      */
-    public function animeBelongToManyUsers() {
-        $anime = Anime::factory()
-            ->has(User::factory()->count(3))
-            ->create();
+    public function animeBelongToUser() {
+        // Given
+        $user = User::factory(['id' => 12])->create();
+        $anime = Anime::factory(['id' => 11])->create();
+
+        // When
+        $anime->users()->save($user, ['like' => true, 'watch' => true]);
+
+        // Then
+        $this->assertDatabaseCount('users', 1);
+        $this->assertDatabaseHas('anime_user', [
+            'anime_id' => $anime->id,
+            'user_id' => $user->id
+        ]);
+        $this->assertTrue($anime->users[0]->stat_anime->like);
+        $this->assertTrue($anime->users[0]->stat_anime->watch);
+        $this->assertNotTrue($anime->users[0]->stat_anime->want_to_watch);
     }
 }
